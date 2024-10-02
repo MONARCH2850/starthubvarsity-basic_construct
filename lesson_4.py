7405ac8c0aa4b5016b6de1eb37a910e2ee5770ce
monarch2850 <jonathanajie1@gmail.com>
2024-10-02 12:15:17 +0630

    first commit

---
+++ lesson_4.py
@@ -0,0 +1,13 @@
+def Jonathan_Ajie():
+    name= "Jonathan"
+    # my name is Jonathan
+    hieght= "184cm"
+    # I am a tall guy
+    age= 21
+    # lol
+    is_male= True
+    # I am an alpha male
+    print(name, hieght, age, is_male)
+    # unseen code
+
+Jonathan_Ajie()
